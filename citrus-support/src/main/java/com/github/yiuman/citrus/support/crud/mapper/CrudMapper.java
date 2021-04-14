83cb6aa7d841b4ff572b59fce37b226f026b6415
yiumankam <415481084@qq.com>
2021-04-14 23:16:34 +0800

    重构CrudMapper,增加批量删除

--- citrus-support/src/main/java/com/github/yiuman/citrus/support/crud/mapper/CrudMapper.java
+++ citrus-support/src/main/java/com/github/yiuman/citrus/support/crud/mapper/CrudMapper.java
@@ -17,7 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
@@ -61,49 +64,78 @@ public interface CrudMapper<T> extends BaseMapper<T> {
      * @return 保存成功返回true，否则false
      */
     @Transactional(rollbackFor = Exception.class)
-    default boolean saveBatch(Collection<T> entityList) {
-        if (CollectionUtils.isEmpty(entityList)) {
-            return false;
-        }
-        Optional<T> first = entityList.stream().findFirst();
-        if (!first.isPresent()) {
-            return false;
+    default boolean saveBatch(Collection<T> entities) {
+        if (Objects.isNull(entities) || entities.isEmpty()) {
+            return true;
         }
 
-        T entity = first.get();
+        T entity = entities.stream().findAny().get();
         Class<?> realEntityClass = ClassUtils.getRealClass(entity.getClass());
         TableInfo tableInfo = TableInfoHelper.getTableInfo(realEntityClass);
         Assert.notNull(tableInfo, "error: can not execute. because can not find cache of TableInfo for entity!");
         //根据是否存在主键进行分组，有主键为true,没主键为false
-        Map<Boolean, List<T>> collect = entityList.stream().collect(Collectors.groupingBy((item) -> {
+        Map<Boolean, List<T>> collect = entities.stream().collect(Collectors.groupingBy((item) -> {
             Object keyFieldValue = ReflectionKit.getFieldValue(item, tableInfo.getKeyProperty());
             //已经存在与数据库
             return StringUtils.checkValNull(keyFieldValue)
                     || Objects.isNull(selectById((Serializable) keyFieldValue));
         }));
 
-        Class<?> realThisClass = ClassUtils.getRealClass(this.getClass());
-        boolean result = true;
-        //insert分组
-        List<T> inserts = collect.get(true);
-        if (!CollectionUtils.isEmpty(inserts)) {
-            String sqlStatement = SqlHelper.getSqlStatement(realThisClass, SqlMethod.INSERT_ONE);
-            result &= executeBatch(inserts, (sqlSession, handlerEntity) -> sqlSession.insert(sqlStatement, handlerEntity));
-        }
-        //update分组
-        List<T> updates = collect.get(false);
-        if (!CollectionUtils.isEmpty(updates)) {
-            String sqlStatement = SqlHelper.getSqlStatement(realThisClass, SqlMethod.UPDATE_BY_ID);
-            result &= executeBatch(updates, (sqlSession, handlerEntity) -> {
-                MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap<>();
-                param.put(Constants.ENTITY, handlerEntity);
-                sqlSession.update(sqlStatement, param);
-            });
-        }
+        return insertBatch(collect.get(true)) && updateBatch(collect.get(false));
 
-        return result;
+    }
 
-//        entityList.forEach(LambdaUtils.consumerWrapper(this::saveEntity));
+    /**
+     * 批量插入
+     *
+     * @param entities 实体集合
+     * @return 集合为空或插入成功返回true 插入失败返回false
+     */
+    @Transactional(rollbackFor = Exception.class)
+    default boolean insertBatch(Collection<T> entities) {
+        return CollectionUtils.isEmpty(entities)
+                || executeBatch(entities,
+                (sqlSession, handlerEntity) -> sqlSession.insert(
+                        SqlHelper.getSqlStatement(ClassUtils.getRealClass(this.getClass()), SqlMethod.INSERT_ONE),
+                        handlerEntity
+                ));
+    }
+
+    /**
+     * 批量更新
+     *
+     * @param entities 实体集合
+     * @return 集合为空或更新成功返回true 更新失败返回false
+     */
+    @Transactional(rollbackFor = Exception.class)
+    default boolean updateBatch(Collection<T> entities) {
+        return CollectionUtils.isEmpty(entities)
+                || executeBatch(entities,
+                (sqlSession, handlerEntity) -> {
+                    MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap<>();
+                    param.put(Constants.ENTITY, handlerEntity);
+                    sqlSession.update(SqlHelper.getSqlStatement(ClassUtils.getRealClass(this.getClass()), SqlMethod.UPDATE_BY_ID), param);
+                });
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param entities 实体集合
+     * @return 若集合为空或删除成功返回true 否则返回false
+     */
+    @Transactional(rollbackFor = Exception.class)
+    default boolean deleteBatch(Collection<T> entities) {
+        if (Objects.isNull(entities) || entities.isEmpty()) {
+            return true;
+        }
+        Class<?> realEntityClass = ClassUtils.getRealClass(entities.stream().findAny().get().getClass());
+        return deleteBatchIds(entities.stream().map(item ->
+                (Serializable) ReflectionKit.getFieldValue(
+                        item,
+                        TableInfoHelper.getTableInfo(ClassUtils.getRealClass(realEntityClass)).getKeyProperty()))
+                .filter(Objects::nonNull).collect(Collectors.toList()))
+                > 0;
     }
 
     /**
@@ -113,9 +145,10 @@ public interface CrudMapper<T> extends BaseMapper<T> {
      * @param consumer SqlSession消费者，用户处理单单个实体
      * @return 是否执行成功 true/false
      */
+    @Transactional(rollbackFor = Exception.class)
     default boolean executeBatch(Collection<T> entities, BiConsumer<SqlSession, T> consumer) {
-        if(Objects.isNull(entities) || entities.isEmpty()){
-            return false;
+        if (Objects.isNull(entities) || entities.isEmpty()) {
+            return true;
         }
 
         Class<?> realEntityClass = ClassUtils.getRealClass(entities.stream().findAny().get().getClass());
