51daa9549330553c5ba2d85c475addeeccba144e
suhigh <44283667+suhigh@users.noreply.github.com>
2021-07-22 17:56:09 +0800

    同步

--- citrus-security/src/main/java/com/github/yiuman/citrus/security/jwt/JwtUtils.java
+++ citrus-security/src/main/java/com/github/yiuman/citrus/security/jwt/JwtUtils.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Date;
