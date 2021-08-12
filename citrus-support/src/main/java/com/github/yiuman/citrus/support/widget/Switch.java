cff6ad353cd0037c3684539bade03eed959a8e0f
suhigh <44283667+suhigh@users.noreply.github.com>
2021-08-12 15:15:35 +0800

    bug修正

---
+++ citrus-support/src/main/java/com/github/yiuman/citrus/support/widget/Switch.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package com.github.yiuman.citrus.support.widget;
+
+/**
+ * @author xmen
+ * 2021年7月29日
+ */
+public class Switch extends BaseWidget<Boolean> {
+
+	@Override
+	public String getWidgetName() {
+		return "v-switch";
+	}
+
+	/**
+	 * @param text
+	 * @param key
+	 * @param model
+	 */
+	public Switch(String text, String key) {
+		super(text, key, null);		
+	}
+
+	
+}
