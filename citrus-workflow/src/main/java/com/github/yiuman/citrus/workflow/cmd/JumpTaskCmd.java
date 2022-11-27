5263e04881bdc230fb5fe49e54a313afbde6de33
yiumankam <415481084@qq.com>
2022-11-27 22:24:10 +0800

    fix: 完善多实例任务下节点跳转的问题

--- citrus-workflow/src/main/java/com/github/yiuman/citrus/workflow/cmd/JumpTaskCmd.java
+++ citrus-workflow/src/main/java/com/github/yiuman/citrus/workflow/cmd/JumpTaskCmd.java
@@ -1,5 +1,6 @@
 package com.github.yiuman.citrus.workflow.cmd;
 
+import cn.hutool.core.util.StrUtil;
 import com.github.yiuman.citrus.workflow.exception.WorkflowException;
 import lombok.Builder;
 import org.activiti.bpmn.model.FlowElement;
@@ -8,6 +9,7 @@ import org.activiti.engine.ActivitiEngineAgenda;
 import org.activiti.engine.impl.interceptor.Command;
 import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntityManager;
 import org.activiti.engine.impl.util.ProcessDefinitionUtil;
 
 import java.util.Objects;
@@ -31,16 +33,28 @@ public class JumpTaskCmd implements Command<Void> {
      */
     private final String targetTaskKey;
 
+    private String reason;
+
     public JumpTaskCmd(String executionId, String targetTaskKey) {
         this.executionId = executionId;
         this.targetTaskKey = targetTaskKey;
     }
 
+    public JumpTaskCmd(String executionId, String targetTaskKey, String reason) {
+        this.executionId = executionId;
+        this.targetTaskKey = targetTaskKey;
+        this.reason = reason;
+    }
+
     @Override
     public Void execute(CommandContext commandContext) {
-
         //1.找到当前执行实例
-        ExecutionEntity executionEntity = commandContext.getExecutionEntityManager().findById(executionId);
+        ExecutionEntityManager executionEntityManager = commandContext.getExecutionEntityManager();
+        ExecutionEntity executionEntity = executionEntityManager.findById(executionId);
+        String parentExecutionId = executionEntity.getParentId();
+        if (StrUtil.isNotBlank(parentExecutionId) && !Objects.equals(parentExecutionId, executionEntity.getParentProcessInstanceId())) {
+            executionEntity = executionEntityManager.findById(parentExecutionId);
+        }
         //2.找到当前的流程
         Process process = ProcessDefinitionUtil.getProcess(executionEntity.getProcessDefinitionId());
         FlowElement targetFlowElement = process.getFlowElement(targetTaskKey);
@@ -49,13 +63,13 @@ public class JumpTaskCmd implements Command<Void> {
         }
 
         //3.将当前的执行实例活动删除，删除原因为jump
-        commandContext.getHistoryManager().recordActivityEnd(executionEntity, "jump");
+        commandContext.getHistoryManager().recordActivityEnd(executionEntity, reason);
         //4.将目标节点设置到当前的执行实例中
         executionEntity.setCurrentFlowElement(targetFlowElement);
 
         //5.删除当前执行中的任务
         commandContext.getTaskEntityManager().findTasksByExecutionId(executionId)
-                .forEach(task -> commandContext.getTaskEntityManager().deleteTask(task, "jump", false, true));
+                .forEach(task -> commandContext.getTaskEntityManager().deleteTask(task, reason, false, true));
 
         //6.触发实例流转
         ActivitiEngineAgenda agenda = commandContext.getAgenda();
